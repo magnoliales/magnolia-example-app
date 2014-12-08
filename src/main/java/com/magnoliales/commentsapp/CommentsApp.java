f85e398bdebe9f1af1b011742691b9d398c36b5f
vkartashov <kartashov.vasily@googlemail.com>
2014-12-08 12:09:28 +0000

    simple layout added

--- src/main/java/com/magnoliales/commentsapp/CommentsApp.java
+++ src/main/java/com/magnoliales/commentsapp/CommentsApp.java
@@ -14,6 +14,4 @@ public class CommentsApp extends BaseApp {
     public CommentsApp(AppContext appContext, AppView view) {
         super(appContext, view);
     }
-
-
 }
--- src/main/java/com/magnoliales/commentsapp/browser/CommentsBrowserSubAppView.java
+++ src/main/java/com/magnoliales/commentsapp/browser/CommentsBrowserSubAppView.java
@@ -1,19 +1,54 @@
 package com.magnoliales.commentsapp.browser;
 
-import com.vaadin.ui.Component;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.server.Sizeable;
+import com.vaadin.ui.*;
 import info.magnolia.ui.api.view.View;
+
+import java.util.Date;
 
 public class CommentsBrowserSubAppView implements View {
 
-    private VerticalLayout layout;
+    public enum Status { NEW, APPROVED, REJECTED };
 
+    private VerticalLayout layout;
+    private int counter;
+
+    /**
+     * @todo Add column generator for the status
+     * @todo Move search box to the right
+     * @todo Connect to the workspace
+     * @todo See how to make use of dialogs in Magnolia from Vaadin application
+     * @todo Implement search to parametrise the search box
+     */
     public CommentsBrowserSubAppView() {
+
+        final Button button = new Button();
+        final Table table = new Table();
+
+        button.setCaption("Click me");
+        button.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                table.addItem(new Object[] {
+                    "Date", new Date().toString(), Status.APPROVED
+                }, counter++);
+            }
+        });
+
+        table.addContainerProperty("Title", String.class, null);
+        table.addContainerProperty("Author", String.class, null);
+        table.addContainerProperty("Status", Status.class, Status.NEW);
+        table.setSizeFull();
+
         layout = new VerticalLayout();
         layout.setMargin(true);
         layout.setSpacing(true);
-        layout.addComponent(new Label("There we go"));
+        layout.setSizeFull();
+
+        layout.addComponent(button);
+        layout.addComponent(table);
+
+        layout.setExpandRatio(table, 1);
     }
 
     @Override
